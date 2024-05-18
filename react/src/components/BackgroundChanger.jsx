dc7d031e93000143b0de35ed57daf69dc80cdcd6
neobugs1 <n.smileski@gmail.com>
2024-05-18 22:46:18 +0200

    add background image transition

--- react/src/components/BackgroundChanger.jsx
+++ react/src/components/BackgroundChanger.jsx
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { Box, Image } from "@chakra-ui/react";
-import image1 from "../assets/wood-industry-l.wsQzoRP4.jpg";
+import image1 from "../assets/metalindustry.jpg";
+import image2 from "../assets/logistics.jpg";
 
 const BackgroundChanger = ({ children }) => {
   const images = [
     image1,
+    image2,
     // Add more image URLs
   ];
 
-  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImage((prevImage) => {
-        const currentIndex = images.indexOf(prevImage);
-        const nextIndex = (currentIndex + 1) % images.length;
-        return images[nextIndex];
-      });
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 5000); // Change image every 5 seconds
 
     return () => clearInterval(interval);
   }, [images]);
 
   return (
-    <Box bgImage={currentImage} bgSize="cover" bgPosition="center" bgRepeat="no-repeat">
+    <Box
+      bgImage={images[currentImageIndex]}
+      bgSize="cover"
+      bgPosition="center"
+      position="absolute"
+      transition="background-image 1s ease-in"
+      sx={{
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
       {children}
     </Box>
   );
