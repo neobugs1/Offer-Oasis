9e66e381477bff2f3e4dae779aa951395c883d55
neobugs1 <n.smileski@gmail.com>
2024-05-21 21:15:50 +0200

    add Search Page

--- react/src/views/SearchPage.jsx
+++ react/src/views/SearchPage.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import SearchBar from "../components/SearchBar";
+
+const categories = [
+  "Electronics",
+  "Computers",
+  "Smart Home",
+  "Gaming",
+  "Office Solutions",
+  // Add more categories as needed
+];
+
+const handleSearch = (searchTerm, category) => {
+  console.log("Search Term:", searchTerm);
+  console.log("Selected Category:", category);
+  // Implement the search functionality here
+};
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <Box p={6}>
+      <SearchBar onSearch={handleSearch} />
+      {/* Additional content */}
+    </Box>
+  );
 };
 
 export default SearchPage;
